7feaca8af44abb8fa8d1aacda5e16cee1e2f16a5
Ren_ <58993316+renzopereyra115@users.noreply.github.com>
2023-04-14 16:59:12 -0700

    Updated main.cpp

--- Team 2/src/main.cpp
+++ Team 2/src/main.cpp
@@ -1,5 +1,6 @@
 //
 // Created by Jorge Enriquez on 3/17/23.
+// Updated by Renzo Pereyra on 04/14/23.
 //
 
 #include "vex.h"
@@ -20,6 +21,9 @@ motor LeftRear = motor(PORT2, ratio36_1, false);
 motor RightFront = motor(PORT3, ratio18_1, true);
 motor RightRear = motor(PORT4, ratio18_1, true);
 motor arm = motor(PORT5, ratio18_1, true);
+float WheelCircumference = (4.0 * 3.14159265); //Diameter of wheel * Pi
+float RevToAngle = (1/93); // 1 revolution turns 93 degrees
+int TurningSpeed = 45;
 
 
 /*---------------------------------------------------------------------------*/
@@ -50,11 +54,43 @@ void pre_auton(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
+void goFwd(float distance, int speed){
+    float MotorDegrees = (distance /WheelCircumference)* 360;
+    leftFront.spin(fwd,MotorDegrees,deg,speed,pct,false);
+    leftRear.spin(fwd,MotorDegrees,deg,speed,pct,false);
+    rightFront.spin(fwd,MotorDegrees,deg,speed,pct,false);
+    rightRear.spin(fwd,MotorDegrees,deg,speed,pct);
+}
+void goBack(float distance, int speed){
+    float MotorDegrees = (distance /WheelCircumference)* 360;
+    leftFront.spin(rev,MotorDegrees,deg,speed,pct,false);
+    leftRear.spin(rev,MotorDegrees,deg,speed,pct,false);
+    rightFront.spin(rev,MotorDegrees,deg,speed,pct,false);
+    rightRear.spin(rev,MotorDegrees,deg,speed,pct);
+}
+void turnLeft(float degrees){
+    //turn MotorDegrees into RobotDegrees by testing
+    float NumberOfDegrees = degrees * RevToAngle * 360;
+    leftFront.spin(rev,NumberOfDegrees,def,TurningSpeed,pct, false);
+    leftRear.spin(rev,NumberOfDegrees,def,TurningSpeed,pct, false);
+    rightFront.spin(fwd,NumberOfDegrees,def,TurningSpeed,pct, false);
+    rightRear.spin(fwd,NumberOfDegrees,def,TurningSpeed,pct);
+}
+void turnRight(float degrees){
+    //turn MotorDegrees into RobotDegrees by testing
+    float NumberOfDegrees = degrees * RevToAngle * 360;
+    leftFront.spin(fwd,NumberOfDegrees,def,TurningSpeed,pct, false);
+    leftRear.spin(fwd,NumberOfDegrees,def,TurningSpeed,pct, false);
+    rightFront.spin(rev,NumberOfDegrees,def,TurningSpeed,pct, false);
+    rightRear.spin(rev,NumberOfDegrees,def,TurningSpeed,pct);
+}
 
 void autonomous(void) {
-    // ..........................................................................
-    // Insert autonomous user code here.
-    // ..........................................................................
+    //int counter = 1;
+    //int power = 11200;
+
+    //robot1.fly1.move_voltage(power);
+    goFwd(10,50); //Go forward 10 inches at a speed of 50
 }
 
 /*---------------------------------------------------------------------------*/
